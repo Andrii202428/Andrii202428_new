c9a3b96038eff69550bffa071377a3f0287cd6d3
Andrii202428 <shefreports@gmail.com>
2026-02-01 21:28:14 +0200

    Update greeting message in hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ import os
 current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 print("--- ОТЧЕТ РОБОТА ---")
-print(f"Привет, Andrii! Код запущен успешно.")
+print(f"Привет, Andriiiiiii! Код запущен успешно.")
 print(f"Время запуска на сервере: {current_time}")
 print(f"Операционная система сервера: {os.name}")
 print("--------------------")
